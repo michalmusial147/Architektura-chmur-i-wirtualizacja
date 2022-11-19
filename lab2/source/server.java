f29c9eb4f5c08f85ccc33b4de5d87c75607432c5
michalmusial147 <michal147147@gmail.com>
2022-11-19 14:27:18 +0100

    Dodano obsługę querry params.

--- lab2/source/server.java
+++ lab2/source/server.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Date;  
- 
+import java.util.Map; 
+import java.util.HashMap;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -20,24 +21,57 @@ public class Test {
         server.start();
     }
 
+    public static Map<String, String> queryToMap(String query) {
+        if(query == null) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
+
     static class MyHandler implements HttpHandler {
     	
     	
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World from java!\n";
-            Instant nowUtc = Instant.now();
-	    ZoneId warsaw = ZoneId.of("Europe/Warsaw");
-            ZonedDateTime nowWarsaw = ZonedDateTime.ofInstant(nowUtc, warsaw);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-	    String formattedString = nowWarsaw.format(formatter);
-            response = response.concat(formattedString + "\n");
-            t.sendResponseHeaders(200, response.length());
+            String response = "";
+            Map<String, String> queryMap = queryToMap(t.getRequestURI().getQuery());
+            if(queryMap == null) {
+                System.out.println("Querry param was not provided!");
+                response = "Hello World from java!\n";
+                t.sendResponseHeaders(200, response.length());
+            }
+            else if(queryMap.get("cmd").equals("time")) {
+                Instant nowUtc = Instant.now();
+                ZoneId warsaw = ZoneId.of("Europe/Warsaw");
+                ZonedDateTime nowWarsaw = ZonedDateTime.ofInstant(nowUtc, warsaw);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                String formattedString = nowWarsaw.format(formatter);
+                response = formattedString;
+                t.sendResponseHeaders(200, response.length());
+            }
+            else if(queryMap.get("cmd").equals("rev")) {
+                response = "rev";
+
+                t.sendResponseHeaders(200, response.length());
+            }
+
+            
+            
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
-	    System.out.println("Served hello world...");
+	   
         }
     }
 
 }
+
